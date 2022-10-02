9af543f5026162c9c7b32d61b0b9f4d3b416f412
PNAleshkin <vinci.aleshkin@gmail.com>
2022-10-02 19:24:28 +0300

    feat: add local fonts gilroy and nekst, update title of the page and add h1

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1 class="title">Рассчитайте стоимость автомобиля в лизинг</h1>
 </template>
 
 <script lang="ts">
@@ -12,10 +12,26 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@font-face {
+  font-family: "Nekst";
+  src: local("Nekst"),
+    url("@/assets/fonts/nekst/Nekst-Bold.woff2") format("woff2"),
+    url("@/assets/fonts/nekst/Nekst-Bold.woff") format("woff"),
+    url("@/assets/fonts/nekst/Nekst-Bold.ttf") format("ttf"),
+    url("@/assets/fonts/nekst/Nekst-Bold.eot") format("eot");
+  font-style: normal;
+}
+@font-face {
+  font-family: "Gilroy";
+  src: local("Gilroy"),
+    url("@/assets/fonts/gilroy/Gilroy-Bold.woff2") format("woff2"),
+    url("@/assets/fonts/gilroy/Gilroy-Bold.woff") format("woff"),
+    url("@/assets/fonts/gilroy/Gilroy-Bold.ttf") format("ttf"),
+    url("@/assets/fonts/gilroy/Gilroy-Bold.eot") format("eot");
+  font-style: normal;
+}
+.title {
+  font-family: "Nekst", "Gilroy";
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
